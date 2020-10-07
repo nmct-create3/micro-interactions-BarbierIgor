8a22fb17699d4e4ddf07757852afd4efe382f9fa
BarbierIgor <55786877+BarbierIgor@users.noreply.github.com>
2020-10-07 09:37:30 +0200

    password

--- script/app.js
+++ script/app.js
@@ -1,9 +1,28 @@
+let email = {},
+    password = {},
+    signInButton;
+
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    
+    let checkbox = document.querySelector(`.c-toggle-password__checkbox`);
+    let passwordInput = document.querySelector(`.c-toggle-password__input`);
+    checkbox.addEventListener(`change`, function(){
+
+        if(checkbox.checked){
+            passwordInput.type = "text";
+            console.log("checked")
+        }
+        else{
+            passwordInput.type = "password";
+            console.log("unchecked")
+        }
+        
+
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
